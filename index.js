61c5a976be733486fd98105b19d95fd808cb1daf
jefaokpta <jefaokpta@hotmail.com>
2023-04-20 17:39:30 -0300

    dev - listou s3

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const AWS = require('aws-sdk');
+const S3 = new AWS.S3();
+
 module.exports.run = async (event, context) => {
   const time = new Date();
   console.log(`Sua funcao CRON "${context.functionName}" rodou em ${time}`);
+  // list all buckets
+
+    await S3.listBuckets().promise()
+        .then(data => console.log(data))
 };
